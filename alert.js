04462214a4f906c6f11711098c07d240f36fda80
nakagawa <Takuya.Nakagawa@kusurinomadoguchi.co.jp>
2021-12-28 15:54:56 +0900

    first comitt

---
+++ alert.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("今日も楽しく開発しよう！");
+}
+
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: showAlert
+    });
+});
